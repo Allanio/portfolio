3ffd9cafce94ce5aeaa6d4780c7ef2bf27ebdbb6
Allan Kadhem <allan.kadhem@tesco.com>
2019-12-20 14:54:36 +0000

    Updates

---
+++ precache-manifest.084ec8d77b123a33c3290abcda914e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229518a3be9285154b9f79fc5a31a203",
+    "url": "/allankadhem/index.html"
+  },
+  {
+    "revision": "4c5817cf74d443ccd079",
+    "url": "/allankadhem/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4fad62abd374e2dac007",
+    "url": "/allankadhem/static/js/2.f524bf96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/allankadhem/static/js/2.f524bf96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c5817cf74d443ccd079",
+    "url": "/allankadhem/static/js/main.d5b62ac3.chunk.js"
+  },
+  {
+    "revision": "34ad668be1aa6cb4733e",
+    "url": "/allankadhem/static/js/runtime-main.a77df20b.js"
+  }
+]);
