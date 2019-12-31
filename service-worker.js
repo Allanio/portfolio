35692ae5df6483dda845394f62cc41a28f86b595
Allan Kadhem <allan.kadhem@tesco.com>
2019-12-31 16:40:41 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.48d1800b57184a4405e73bbd211d9631.js"
+  "/portfolio/precache-manifest.bb3178371a5eb0bf25a0a51f3157a3f2.js"
 );
 
 self.addEventListener('message', (event) => {
